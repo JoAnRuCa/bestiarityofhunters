f43ff584fd7702827b1aecccaf936e72ef32838c
JoAnRuCa <joseantrc1998@gmail.com>
2026-02-02 12:11:15 +0100

    desplegable arreglado

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,25 +1,53 @@
 <header>
     <div class="logo">
-      <img src="" alt="Logo">
+        <img src="" alt="Logo">
     </div>
+
     <nav>
-      <ul>
-        <li><a href="MU_Inicio.html">Inicio</a></li>
-        <li class="dropdown">
-          <a href="#">Categorías</a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Opción 1</a></li>
-            <li><a href="#">Opción 2</a></li>
-            <li><a href="#">Opción 3</a></li>
-          </ul>
-        </li>
-        <li><a href="MU_Productos.html">Productos</a></li>
-        <li><a href="MU_Sucursales.html">Sucursales</a></li>
-        <li><a href="MU_Contacto.html">Contacto</a></li>
-      </ul>
+        <ul>
+            <li><a href="/">Inicio</a></li>
+
+            <li class="dropdown">
+                <a href="#">Categorías</a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Opción 1</a></li>
+                    <li><a href="#">Opción 2</a></li>
+                    <li><a href="#">Opción 3</a></li>
+                </ul>
+            </li>
+
+            <li><a href="/productos">Productos</a></li>
+            <li><a href="/sucursales">Sucursales</a></li>
+            <li><a href="/contacto">Contacto</a></li>
+        </ul>
     </nav>
+
+    {{-- Zona derecha del header --}}
     <div>
-      <input type="button" class="login" value="Login">
-      <input type="button" class="register" value="Register">
+        @guest
+            <a href="{{ route('login') }}" class="login">Login</a>
+            <a href="{{ route('register') }}" class="register">Register</a>
+        @endguest
+
+@auth
+  <div class="dropdown dropdown-user">
+    <a href="#">{{ Auth::user()->name }}</a>
+    <ul class="dropdown-menu">
+      <li>
+        <a href="#"
+           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+          Logout
+        </a>
+      </li>
+    </ul>
+  </div>
+
+  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:none;">
+    @csrf
+  </form>
+@endauth
+
+
+
     </div>
-  </header>
+</header>
