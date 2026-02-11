36fdc2677bf76197ea405ec6e40ec9c2c2c61839
JoAnRuCa <joseantrc1998@gmail.com>
2026-02-11 11:38:07 +0100

    modelos relacionados builds

--- app/Models/BuildsEquipment.php
+++ app/Models/BuildsEquipment.php
@@ -7,5 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class BuildsEquipment extends Model
 {
-    use HasFactory;
+    protected $fillable = ['build_id', 'equipment_id', 'tipo'];
+
+// Relación hacia arriba (a qué Build pertenece)
+public function build()
+{
+    return $this->belongsTo(Build::class, 'build_id');
+}
+
+// Relación hacia abajo (qué decoraciones tiene esta pieza)
+public function decorations()
+{
+    return $this->hasMany(BuildsEquipmentsDecoration::class, 'build_equipment_id');
+}
+
+use HasFactory;
 }
