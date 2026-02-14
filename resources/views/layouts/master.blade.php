6f9029ba3d4b409d4a63552f82ecdd0349ae3974
JoAnRuCa <joseantrc1998@gmail.com>
2026-02-14 12:21:24 +0100

    favicon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,14 +3,19 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>@yield('title')</title>
-
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}?v={{ time() }}">
-  <link rel="stylesheet" href="{{ asset('css/master.css') }}?v={{ time() }}">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
+  {{-- Favicon --}}
+  <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
+  <link rel="apple-touch-icon" href="{{ asset('images/favicon.png') }}"> {{-- Para móviles --}}
+
+  <title>@yield('title') | Hunter's Bestiary</title>
+
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap" rel="stylesheet">
   
+  {{-- Estilos con control de caché --}}
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}?v={{ time() }}">
+  <link rel="stylesheet" href="{{ asset('css/master.css') }}?v={{ time() }}">
 </head>
 
 <body class="flex flex-col min-h-screen" style="font-family: 'Inter', sans-serif;">
